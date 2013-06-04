904ff7528d53f357a367992e32863c54ac8b381d
Radford Neal <radfordneal@gmail.com>
2013-06-04 16:50:41 -0400

    remove unused variables

--- src/main/eval.c
+++ src/main/eval.c
@@ -1981,7 +1981,7 @@ SEXP attribute_hidden promiseArgsWithValues(SEXP el, SEXP rho, SEXP values)
 
 SEXP attribute_hidden promiseArgsWith1Value(SEXP el, SEXP rho, SEXP value)
 {
-    SEXP s, a, b;
+    SEXP s;
     PROTECT(s = promiseArgs(el, rho));
     if (s == R_NilValue) error(_("dispatch error"));
     if (TYPEOF(CAR(s)) == PROMSXP)
