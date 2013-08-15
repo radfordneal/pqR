941abcf9a27a20d41b1713ac3d72ada84f558f66
Radford Neal <radfordneal@gmail.com>
2013-08-14 21:59:54 -0400

    when helper threads exist, merge only + and -

--- src/extra/helpers/helpers-app.h
+++ src/extra/helpers/helpers-app.h
@@ -114,11 +114,18 @@ extern char *Rf_var_name (helpers_var_ptr);
 
 #define MAX_OPS_MERGED 3      /* Must be from 2 to 6 */
 
-extern helpers_task_proc task_merged_arith_math1;
+extern helpers_task_proc task_merged_arith_math1,
+                         task_real_arithmetic,
+                         task_unary_minus;
 
 #define helpers_can_merge(out,proc_a,op_a,in1_a,in2_a,proc_b,op_b,in1_b,in2_b) \
-  ((proc_b) != task_merged_arith_math1 || \
-   ((op_b) & (0xff<<(8*MAX_OPS_MERGED))) == 0)
+  ( helpers_not_multithreading ? \
+      (proc_b) != task_merged_arith_math1 \
+       || ((op_b) & (0xff<<(8*MAX_OPS_MERGED))) == 0 \
+    : (proc_a) == task_real_arithmetic && op_a <= MINUSOP \
+       || (proc_b) == task_real_arithmetic && op_b <= MINUSOP \
+       || (proc_a) == task_unary_minus || (proc_b) == task_unary_minus \
+  )
 
 extern void helpers_merge_proc ( /* helpers_var_ptr out, */
   helpers_task_proc *proc_A, helpers_op_t op_A, 
