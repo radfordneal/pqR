cf06b52b29989b189f3e12f3a3e9f88ef238844e
Radford Neal <radfordneal@gmail.com>
2014-02-10 23:26:10 -0500

    fixed bug involving deletion of pairlist elements, added tests for such

--- tests/subsetting.R
+++ tests/subsetting.R
@@ -14,3 +14,27 @@ class(a) <- "fred"
     { stopifnot(identical(n,substitute(n))); x[[n]] <- value; x }
 a$q <- a$q + 1
 print(a)
+
+# Replacement of pairlist or language elements.
+
+L <- pairlist(1,2,3)
+L[[2]] <- 9
+stopifnot(identical(L,pairlist(1,9,3)))
+L[[2]] <- NULL
+stopifnot(identical(L,pairlist(1,3)))
+L[[1]] <- NULL
+stopifnot(identical(L,pairlist(3)))
+
+L <- pairlist(a=1,b=2,c=3)
+L$b <- 9
+stopifnot(identical(L,pairlist(a=1,b=9,c=3)))
+L$b <- NULL
+stopifnot(identical(L,pairlist(a=1,c=3)))
+L$a <- NULL
+stopifnot(identical(L,pairlist(c=3)))
+
+L <- parse(text="f(1,2)")[[1]]
+L[[2]] <- 3
+stopifnot(identical(L,parse(text="f(3,2)")[[1]]))
+L[[1]] <- NULL
+stopifnot(identical(L,pairlist(3,2)))
