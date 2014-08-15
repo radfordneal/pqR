2cc16bd4e1823cf118a28d60528a03400efed485
Radford Neal <radfordneal@gmail.com>
2014-08-15 13:05:13 -0400

    have ScalarXXX use specialized allocVector1

--- src/main/memory.c
+++ src/main/memory.c
@@ -331,7 +331,8 @@ static const char *sexptype2char(SEXPTYPE type) {
 static int gc_force_wait = 0;
 static int gc_force_gap = 0;
 static Rboolean gc_inhibit_release = FALSE;
-#define FORCE_GC (gc_force_wait > 0 ? (--gc_force_wait > 0 ? 0 : (gc_force_wait = gc_force_gap, 1)) : 0)
+#define FORCE_GC (gc_force_wait > 0 ? \
+  (--gc_force_wait > 0 ? 0 : (gc_force_wait = gc_force_gap, 1)) : 0)
 
 #define GC_PROT(X) do { \
     int __wait__ = gc_force_wait; \
@@ -866,7 +867,8 @@ static R_size_t R_NodesInUse = 0;
 #endif
 
 
-/* Node Allocation. */
+/* Node Allocation.  Initializes sxpinfo to zeros (except node class set 
+   as passed), and ATTRIB to R_NilValue.  Other fields are not initialized. */
 
 static R_INLINE SEXP get_free_node (int c)
 {
@@ -874,9 +876,8 @@ static R_INLINE SEXP get_free_node (int c)
 
     SEXP n = R_GenHeap[c].Free;
     if (n == R_GenHeap[c].New) {
-        GetNewPage(c);
+        GetNewPage(c); /* guaranteed to provide a free node (or gives error) */
         n = R_GenHeap[c].Free;
-        if (n == R_GenHeap[c].New) abort();
     }
     R_GenHeap[c].Free = NEXT_NODE(n);
     R_NodesInUse++;
@@ -889,6 +890,7 @@ static R_INLINE SEXP get_free_node (int c)
 
     n->sxpinfo = UnmarkedNodeTemplate.sxpinfo;
     SET_NODE_CLASS(n,c);
+    ATTRIB(n) = R_NilValue;
     R_SmallNallocSize += NodeClassSize[c];
     return n;
 }
@@ -1183,6 +1185,9 @@ static void GetNewPage(int node_class)
         VALGRIND_MAKE_MEM_DEFINED (&s->sxpinfo, sizeof s->sxpinfo);
     }
 #endif
+
+    if (R_GenHeap[node_class].Free == R_GenHeap[node_class].New)
+        abort(); /* Shouldn't be possible, since page_count should be > 0 */
 }
 
 /* Scan pages, releasing (some) pages with no nodes in use, and (maybe)
@@ -2589,7 +2594,6 @@ SEXP allocSExp(SEXPTYPE t)
     CAR(s) = R_NilValue;
     CDR(s) = R_NilValue;
     TAG(s) = R_NilValue;
-    ATTRIB(s) = R_NilValue;
     return s;
 }
 
@@ -2603,7 +2607,6 @@ static SEXP allocSExpNonCons(SEXPTYPE t)
     }
     s = get_free_node(SEXPREC_class);
     TYPEOF(s) = t;
-    ATTRIB(s) = R_NilValue;
     return s;
 }
 
@@ -2624,7 +2627,6 @@ SEXP cons(SEXP car, SEXP cdr)
     CAR(s) = CHK(car);
     CDR(s) = CHK(cdr);
     TAG(s) = R_NilValue;
-    ATTRIB(s) = R_NilValue;
     return s;
 }
 
@@ -2644,7 +2646,6 @@ SEXP cons_with_tag(SEXP car, SEXP cdr, SEXP tag)
     CAR(s) = CHK(car);
     CDR(s) = CHK(cdr);
     TAG(s) = CHK(tag);
-    ATTRIB(s) = R_NilValue;
     return s;
 }
 
@@ -2685,7 +2686,6 @@ SEXP NewEnvironment(SEXP namelist, SEXP valuelist, SEXP rho)
     FRAME(newrho) = valuelist;
     ENCLOS(newrho) = CHK(rho);
     HASHTAB(newrho) = R_NilValue;
-    ATTRIB(newrho) = R_NilValue;
 
     v = CHK(valuelist);
     n = CHK(namelist);
@@ -2723,35 +2723,56 @@ SEXP attribute_hidden mkPROMISE(SEXP expr, SEXP rho)
     PRCODE(s) = CHK(expr);
     PRENV(s) = CHK(rho);
     PRSEEN(s) = 0;
-    ATTRIB(s) = R_NilValue;
     return s;
 }
 
-/* Allocation of scalars, which compiler may optimize into specialized
-   versions of allocVector.  These versions always return an unshared 
-   value. */
+/* Allocation of scalars, using a version of allocVector specialized for
+   vectors of length one.  These versions always return an unshared value. */
+
+static /*R_INLINE*/ SEXP allocVector1 (SEXPTYPE type)
+{
+    /* Note that "type" must be RAWSXP, LGLSXP, INTSXP, or REALSXP, so a
+       vector of length 1 is guaranteed to fit in the first node class, and
+       so that there's no need to initialize a pointer in the data part. */
+
+#if VALGRIND_LEVEL==0
+    if (FORCE_GC || NO_FREE_NODES()) {
+        R_gc_internal(0);
+        if (NO_FREE_NODES())
+            mem_err_cons();
+    }
+    SEXP s = get_free_node(0);
+    TYPEOF(s) = type;
+    LENGTH(s) = 1;
+    if (R_IsMemReporting && !R_MemPagesReporting)
+        R_ReportAllocation (
+          sizeof(SEXPREC_ALIGN) + NodeClassSize[0] * sizeof(VECREC), type, 1);
+    return s;
+#else
+    return allocVector (type, 1);
+#endif
+}
 
 SEXP ScalarLogical(int x)
 {
-    SEXP ans = allocVector(LGLSXP, 1);
+    SEXP ans = allocVector1(LGLSXP);
     LOGICAL(ans)[0] = x == 0 || x == NA_LOGICAL ? x : 1;
     return ans;
 }
 
 SEXP ScalarInteger(int x)
 {
-    SEXP ans = allocVector(INTSXP, 1);
+    SEXP ans = allocVector1(INTSXP);
     INTEGER(ans)[0] = x;
     return ans;
 }
 
 SEXP ScalarReal(double x)
 {
-    SEXP ans = allocVector(REALSXP, 1);
+    SEXP ans = allocVector1(REALSXP);
     REAL(ans)[0] = x;
     return ans;
 }
-
 
 SEXP ScalarComplex(Rcomplex x)
 {
@@ -2772,7 +2793,7 @@ SEXP ScalarString(SEXP x)
 
 SEXP ScalarRaw(Rbyte x)
 {
-    SEXP ans = allocVector(RAWSXP, 1);
+    SEXP ans = allocVector1(RAWSXP);
     RAW(ans)[0] = x;
     return ans;
 }
@@ -2789,7 +2810,7 @@ SEXP ScalarIntegerMaybeConst(int x)
             return R_ScalarIntegerNA;
     }
 
-    SEXP ans = allocVector(INTSXP, 1);
+    SEXP ans = allocVector1(INTSXP);
     INTEGER(ans)[0] = x;
     return ans;
 }
@@ -2802,15 +2823,15 @@ SEXP ScalarRealMaybeConst(double x)
            as doubles, since double comparison doesn't work for NA or when 
            comparing -0 and +0 (which should be distinct). */
 
-        if (*(uint64_t*) &x == *(uint64_t*) &REAL(R_ScalarRealNA)[0]) 
-            return R_ScalarRealNA;
         if (*(uint64_t*) &x == *(uint64_t*) &REAL(R_ScalarRealZero)[0]) 
             return R_ScalarRealZero;
         if (*(uint64_t*) &x == *(uint64_t*) &REAL(R_ScalarRealOne)[0]) 
             return R_ScalarRealOne;
+        if (*(uint64_t*) &x == *(uint64_t*) &REAL(R_ScalarRealNA)[0]) 
+            return R_ScalarRealNA;
     }
 
-    SEXP ans = allocVector(REALSXP, 1);
+    SEXP ans = allocVector1(REALSXP);
     REAL(ans)[0] = x;
     return ans;
 }
@@ -2834,7 +2855,7 @@ SEXP ScalarStringMaybeConst(SEXP x)
 
 SEXP ScalarRawMaybeConst(Rbyte x)
 {
-    SEXP ans = allocVector(RAWSXP, 1);
+    SEXP ans = allocVector1(RAWSXP);
     RAW(ans)[0] = x;
     return ans;
 }
@@ -2868,7 +2889,6 @@ SEXP allocVector(SEXPTYPE type, R_len_t length)
 	        mem_err_cons();
         }
         s = get_free_node(0);
-        ATTRIB(s) = R_NilValue;
         TYPEOF(s) = type;
         LENGTH(s) = length;
         if (R_IsMemReporting && !R_MemPagesReporting)
@@ -3008,6 +3028,7 @@ SEXP allocVector(SEXPTYPE type, R_len_t length)
         }
         s->sxpinfo = UnmarkedNodeTemplate.sxpinfo;
         SET_NODE_CLASS(s, LARGE_NODE_CLASS);
+        ATTRIB(s) = R_NilValue;
         R_LargeVallocSize += size;
         R_GenHeap[LARGE_NODE_CLASS].AllocCount++;
         R_NodesInUse++;
@@ -3018,7 +3039,6 @@ SEXP allocVector(SEXPTYPE type, R_len_t length)
 #endif
     }
 
-    ATTRIB(s) = R_NilValue;
     TYPEOF(s) = type;
     LENGTH(s) = length;
 
