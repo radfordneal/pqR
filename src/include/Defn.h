2732968c9f4e8a86de5d7a6ab22b326f05f20f2c
Radford Neal <radfordneal@gmail.com>
2015-06-18 22:39:19 -0400

    got rid of unused EVAL (could use EVALV anyway)

--- src/include/Defn.h
+++ src/include/Defn.h
@@ -1584,40 +1584,11 @@ static inline SEXP FIND_VAR_PENDING_OK (SEXP sym, SEXP rho)
 }
 
 
-/* Inline versions of eval and evalv, which checks for SELF_EVAL inline.
-   These also do not decrement evalcount, and so must not be used in a 
+/* Inline version of evalv, which checks for SELF_EVAL inline.
+   Also does not decrement evalcount, and so must not be used in a 
    context where this might result in an uninterruptable loop. */
 
 extern SEXP Rf_evalv2 (SEXP, SEXP, int);
-
-static inline SEXP EVAL (SEXP e, SEXP rho)
-{
-    R_variant_result = 0;
-    R_Visible = TRUE;
-
-    if (SELF_EVAL(TYPEOF(e))) {
-        /* Make sure constants in expressions have maximum NAMEDCNT when
-           used as values, so they won't be modified. */
-        SET_NAMEDCNT_MAX(e);
-        return e;
-    }
-
-    if (TYPEOF(e) == SYMSXP && e != R_DotsSymbol && !DDVAL(e)) {
-        if (LASTSYMENV(e) == rho) {
-            SEXP res = CAR(LASTSYMBINDING(e));
-            if (TYPEOF(res) == PROMSXP) 
-                res = PRVALUE_PENDING_OK(res);
-            if (res != R_MissingArg && res != R_UnboundValue) {
-                if (NAMEDCNT_EQ_0(res))
-                    SET_NAMEDCNT_1(res);
-                WAIT_UNTIL_COMPUTED(res);
-                return res;
-            }
-        }
-    }
-
-    return Rf_evalv2 (e, rho, 0);
-}
 
 static inline SEXP EVALV (SEXP e, SEXP rho, int variant)
 {
