20fb620ffef733ffb2969e0070119d03afde1686
Radford Neal <radfordneal@gmail.com>
2015-08-15 17:28:05 -0400

    more tweaks to rtest.c example in front-ends and R-exts.texi

--- src/gnuwin32/front-ends/rtest.c
+++ src/gnuwin32/front-ends/rtest.c
@@ -1,5 +1,8 @@
 /*
- *  R : A Computer Language for Statistical Data Analysis
+ *  pqR : A pretty quick version of R
+ *  Copyright (C) 2013, 2014, 2015 by Radford M. Neal
+ *
+ *  Based on R : A Computer Language for Statistical Data Analysis
  *  Copyright (C) 1998--2007  R Development Core Team
  *
  *  This program is free software; you can redistribute it and/or modify
