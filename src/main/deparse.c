5a7575df0669f2a9fff7e60b7f3e0488165a3e0a
Radford Neal <radfordneal@gmail.com>
2015-08-29 08:38:36 -0400

    make needsparens functions static

--- src/main/deparse.c
+++ src/main/deparse.c
@@ -516,7 +516,7 @@ curlyahead(SEXP s)
    parenthesized when deparsed.  The symbol op is the outer postfix
    operator (eg, $).  The expression arg is the first operand. */
 
-attribute_hidden Rboolean needsparens_postfix (SEXP op, SEXP arg)
+static Rboolean needsparens_postfix (SEXP op, SEXP arg)
 {
     int op_prec = misc_prec(op);
     int in_prec;
@@ -555,7 +555,7 @@ attribute_hidden Rboolean needsparens_postfix (SEXP op, SEXP arg)
    parenthesized when deparsed.  The symbol op is the outer unary
    operator.  The expression arg is the operand. */
 
-attribute_hidden Rboolean needsparens_unary (SEXP op, SEXP arg)
+static Rboolean needsparens_unary (SEXP op, SEXP arg)
 {
     int op_prec = unary_prec(op);
     int in_prec;
@@ -587,7 +587,7 @@ attribute_hidden Rboolean needsparens_unary (SEXP op, SEXP arg)
    operator.  The expression arg is the operand, on the left if
    left is 1. */
 
-attribute_hidden Rboolean needsparens_binary (SEXP op, SEXP arg, int left)
+static Rboolean needsparens_binary (SEXP op, SEXP arg, int left)
 {
     int op_prec = binary_prec(op);
     int in_prec;
