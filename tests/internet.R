dfe222b72eb5fb77e72d58571cbb5c4037d79592
Radford Neal <radfordneal@gmail.com>
2015-09-03 15:18:38 -0400

    change internet.R tests to not reference R Core urls, other improvements

--- tests/internet.R
+++ tests/internet.R
@@ -11,7 +11,9 @@ if(.Platform$OS.type == "unix" &&
    is.null(nsl("cran.r-project.org"))) q()
 
 # test do_download.
-nrow(available.packages(contrib.url("http://cran.r-project.org")))
+a <- available.packages(contrib.url("ftp://price.utstat.utoronto.ca"))
+colnames(a)
+a["ggplot2","License"]
 
 # test url connections on http
 zz <- url("http://pqR-project.org/internet.html")
@@ -23,8 +25,9 @@ try(zz <- url("http://foo.bar", "r"))
 
 # and via read.table, test http and ftp.
 
-read.table("http://www.stats.ox.ac.uk/pub/datasets/csb/ch11b.dat")
-read.table("ftp://ftp.stats.ox.ac.uk/pub/datasets/csb/ch11b.dat")
+(http_dat <- read.table("http://pqR-project.org/test.dat"))
+(ftp_dat  <- read.table("ftp://price.utstat.utoronto.ca/test.dat"))
+identical(http_dat,ftp_dat)
 
 ## everything from here on is directly over sockets
 if(!capabilities("sockets")) stop("no socket capabilities")
@@ -38,14 +41,16 @@ httpget <- function (url, port = 80)
     rurl <- paste(c("", urlel[-(1:3)]), collapse = "/")
     a <- make.socket(host, port = port)
     on.exit(close.socket(a))
-    headreq <- paste("HEAD", rurl, "HTTP/1.0\r\nConnection: Keep-Alive\r\nAccept: text/plain\r\n\r\n")
+    headreq <- paste("HEAD", rurl, 
+      "HTTP/1.0\r\nConnection: Keep-Alive\r\nAccept: text/plain\r\n\r\n")
     write.socket(a, headreq)
     head <- read.socket(a, maxlen = 8000)
     b <- strsplit(head, "\n")[[1]]
     if (length(grep("200 OK", b[1])) == 0) stop(b[1])
     len <- as.numeric(strsplit(grep("Content-Length", b, value = TRUE),
                                ":")[[1]][2])
-    getreq <- paste("GET", rurl, "HTTP/1.0\r\nConnection: Keep-Alive\r\nAccept: text/plain\r\n\r\n")
+    getreq <- paste("GET", rurl, 
+      "HTTP/1.0\r\nConnection: Keep-Alive\r\nAccept: text/plain\r\n\r\n")
     write.socket(a, getreq)
     junk <- read.socket(a, maxlen = nchar(head))
     data <- ""
@@ -64,7 +69,7 @@ if(nchar(Sys.getenv("http_proxy")) > 0
    || nchar(Sys.getenv("HTTP_PROXY")) > 0) {
     cat("http proxy is set, so skip test of http over sockets\n")
 } else {
-    httpget("http://www.stats.ox.ac.uk/pub/datasets/csb/ch11b.dat")
+    httpget("http://www.cs.utoronto.ca/~radford/test.dat")
 }
 
 finger <- function(user, host = "localhost", port = 79, print = TRUE)
