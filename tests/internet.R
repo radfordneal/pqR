9c3f9d1b1f33f8254ceb2f355a67aaeb12b467bf
Radford Neal <radfordneal@gmail.com>
2015-09-03 15:19:05 -0400

    get rid of finger test in internet.R

--- tests/internet.R
+++ tests/internet.R
@@ -71,23 +71,3 @@ if(nchar(Sys.getenv("http_proxy")) > 0
 } else {
     httpget("http://www.cs.utoronto.ca/~radford/test.dat")
 }
-
-finger <- function(user, host = "localhost", port = 79, print = TRUE)
-{
-    if (!is.character(user))
-        stop("user name must be a string")
-    user <- paste(user,"\r\n")
-    socket <- make.socket(host, port)
-    on.exit(close.socket(socket))
-    write.socket(socket, user)
-    output <- character(0)
-    repeat{
-        ss <- read.socket(socket)
-        if (ss == "") break
-        output <- paste(output, ss)
-    }
-    close.socket(socket)
-    if (print) cat(output)
-    invisible(output)
-}
-try(finger("root"))  ## only works if your site provides a finger daemon
