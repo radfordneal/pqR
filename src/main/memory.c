f05048e5bbb365fbf18f98663614c2baa19ff586
Radford Neal <radfordneal@gmail.com>
2016-07-11 22:52:49 -0400

    mkChar / mkCharCE fix

--- src/main/memory.c
+++ src/main/memory.c
@@ -4451,7 +4451,10 @@ static SEXP do_Rprofmem(SEXP call, SEXP op, SEXP args, SEXP rho)
 
 SEXP mkCharCE(const char *name, cetype_t enc)
 {
-    return mkCharLenCE(name, strlen(name), enc);
+    size_t len =  strlen(name);
+    if (len > INT_MAX)
+	error("R character strings are limited to 2^31-1 bytes");
+    return mkCharLenCE(name, (int) len, enc);
 }
 
 /* no longer used in R but docuented in 2.7.x */
@@ -4462,7 +4465,10 @@ SEXP mkCharLen(const char *name, int len)
 
 SEXP mkChar(const char *name)
 {
-    return mkCharLenCE(name, strlen(name), CE_NATIVE);
+    size_t len =  strlen(name);
+    if (len > INT_MAX)
+	error("R character strings are limited to 2^31-1 bytes");
+    return mkCharLenCE(name, (int) len, CE_NATIVE);
 }
 
 /* CHARSXP hashing follows the hash structure from envir.c, but need separate
