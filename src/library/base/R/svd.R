9f2a8cf48c3712235a30dfde4d79404886f0ec05
Radford Neal <radfordneal@gmail.com>
2016-08-26 11:08:05 -0400

    updates from 2.15.1 to base/R and base/man

--- src/library/base/R/svd.R
+++ src/library/base/R/svd.R
@@ -18,6 +18,9 @@
 svd <- function(x, nu = min(n,p), nv = min(n,p), LINPACK = FALSE)
 {
     x <- as.matrix(x)
+
+    if (!all(is.finite(x))) stop("infinite or missing values in 'x'")
+
     dx <- dim(x)
     n <- dx[1L]
     p <- dx[2L]
@@ -32,8 +35,7 @@ svd <- function(x, nu = min(n,p), nv = min(n,p), LINPACK = FALSE)
         return (ret)
     }
 
-    if (!is.double(x))
-	storage.mode(x) <- "double"
+    storage.mode(x) <- "double"
 
     if (!LINPACK) {
         res <- La.svd(x, nu, nv)
@@ -44,7 +46,11 @@ svd <- function(x, nu = min(n,p), nv = min(n,p), LINPACK = FALSE)
         return (ret)
     }
 
-    if (!all(is.finite(x))) stop("infinite or missing values in 'x'")
+    ## LINPACK only from here on.
+    n <- as.integer(n)
+    if(is.na(n)) stop("invalid nrow(x)")
+    p <- as.integer(p)
+    if(is.na(p)) stop("invalid ncol(x)")
 
     if(nu == 0L) {
 	job <- 0L
@@ -67,26 +73,25 @@ svd <- function(x, nu = min(n,p), nv = min(n,p), LINPACK = FALSE)
 
     v <- if(job == 0L) double() else matrix(0, p, p)
 
-    mn <- min(n,p)
     mm <- min(n+1L,p)
     z <- .Fortran("dsvdc",
-		  as.double(x),
+		  x, # did storage.mode above
 		  n,
 		  n,
 		  p,
-		  d=double(mm),
+		  d = double(mm),
 		  double(p),
-		  u=u,
+		  u = u,
 		  n,
-		  v=v,
+		  v = v,
 		  p,
 		  double(n),
 		  as.integer(job),
-		  info=integer(1L),
-		  DUP=FALSE, PACKAGE="base")[c("d","u","v","info")]
+		  info = integer(1L),
+		  DUP = FALSE, PACKAGE = "base")[c("d","u","v","info")]
     if(z$info)
 	stop(gettextf("error %d in 'dsvdc'", z$info), domain = NA)
-    z$d <- z$d[1L:mn]
+    z$d <- z$d[seq_len(min(n, p))]
     if(nv && nv < p) z$v <- z$v[, 1L:nv, drop = FALSE]
     z[c("d", if(nu) "u", if(nv) "v")]
 }
