baa7e713e20043f92a3ddb6c8523447ea2596242
Radford Neal <radfordneal@gmail.com>
2017-06-06 10:36:07 -0400

    more reorg of relop

--- src/main/relop.c
+++ src/main/relop.c
@@ -502,125 +502,142 @@ SEXP attribute_hidden R_relop (SEXP call, SEXP op, SEXP x, SEXP y,
                 y = coerceVector(y,REALSXP);
             UNPROTECT(2);
             PROTECT2(x,y);
-
-            helpers_op_t codeop = (code<<1) | negate;
-            switch (VARIANT_KIND(variant)) {
-            case VARIANT_AND: 
-                ans = allocVector1LGL();
-                task_relop_and (codeop, ans, x, y); 
-                break;
-            case VARIANT_OR:
-                ans = allocVector1LGL();
-                task_relop_or (codeop, ans, x, y); 
-                break;
-            default:
-                ans = allocVector(LGLSXP,n);
-                task_relop (codeop, ans, x, y); 
-                break;
-            }
-            UNPROTECT(2);
-            return ans;
+            PROTECT3(dims=R_NilValue,xnames=R_NilValue,ynames=R_NilValue);
+            xts = yts = 0;
         }
     }
-    
-    PROTECT_WITH_INDEX(x, &xpi);
-    PROTECT_WITH_INDEX(y, &ypi);
 
-    /* That symbols and calls were allowed was undocumented prior to
-       R 2.5.0.  We deparse them as deparse() would, minus attributes */
-    if ((iS = isSymbol(x)) || typeof_x == LANGSXP) {
-	SEXP tmp = allocVector(STRSXP, 1);
-	PROTECT(tmp);
-	SET_STRING_ELT(tmp, 0, (iS) ? PRINTNAME(x) :
-		       STRING_ELT(deparse1(x, 0, DEFAULTDEPARSE), 0));
-	REPROTECT(x = tmp, xpi);
-        typeof_x = STRSXP;
-	UNPROTECT(1);
-    }
-    if ((iS = isSymbol(y)) || typeof_y == LANGSXP) {
-	SEXP tmp = allocVector(STRSXP, 1);
-	PROTECT(tmp);
-	SET_STRING_ELT(tmp, 0, (iS) ? PRINTNAME(y) :
-		       STRING_ELT(deparse1(y, 0, DEFAULTDEPARSE), 0));
-	REPROTECT(y = tmp, ypi);
-        typeof_y = STRSXP;
-	UNPROTECT(1);
-    }
+    else { /* the general case */
 
-    if (!isVector(x) || !isVector(y)) {
-	if (isNull(x) || isNull(y)) {
-	    UNPROTECT(2);
-	    return allocVector(LGLSXP,0);
-	}
-	errorcall(call,
-	  _("comparison (%d) is possible only for atomic and list types"),
-	  PRIMVAL(op));
-    }
+        PROTECT_WITH_INDEX(x, &xpi);
+        PROTECT_WITH_INDEX(y, &ypi);
 
-    if (typeof_x == EXPRSXP || typeof_y == EXPRSXP)
-	errorcall(call, _("comparison is not allowed for expressions"));
+        /* That symbols and calls were allowed was undocumented prior to
+           R 2.5.0.  We deparse them as deparse() would, minus attributes */
 
-    /* ELSE :  x and y are both atomic or list */
+        if ((iS = isSymbol(x)) || typeof_x == LANGSXP) {
+            SEXP tmp = allocVector(STRSXP, 1);
+            PROTECT(tmp);
+            SET_STRING_ELT(tmp, 0, (iS) ? PRINTNAME(x) :
+                           STRING_ELT(deparse1(x, 0, DEFAULTDEPARSE), 0));
+            REPROTECT(x = tmp, xpi);
+            typeof_x = STRSXP;
+            UNPROTECT(1);
+        }
+        if ((iS = isSymbol(y)) || typeof_y == LANGSXP) {
+            SEXP tmp = allocVector(STRSXP, 1);
+            PROTECT(tmp);
+            SET_STRING_ELT(tmp, 0, (iS) ? PRINTNAME(y) :
+                           STRING_ELT(deparse1(y, 0, DEFAULTDEPARSE), 0));
+            REPROTECT(y = tmp, ypi);
+            typeof_y = STRSXP;
+            UNPROTECT(1);
+        }
 
-    if (LENGTH(x) <= 0 || LENGTH(y) <= 0) {
-	UNPROTECT(2);
-	return allocVector(LGLSXP,0);
-    }
+        if (!isVector(x) || !isVector(y)) {
+            if (isNull(x) || isNull(y)) {
+                UNPROTECT(2);
+                return allocVector(LGLSXP,0);
+            }
+            errorcall(call,
+              _("comparison (%d) is possible only for atomic and list types"),
+              PRIMVAL(op));
+        }
 
-    mismatch = FALSE;
-    xarray = isArray(x);
-    yarray = isArray(y);
-    xts = isTs(x);
-    yts = isTs(y);
-    if (nx > 0 && ny > 0)
-	mismatch = ((nx > ny) ? nx % ny : ny % nx) != 0;
+        if (typeof_x == EXPRSXP || typeof_y == EXPRSXP)
+            errorcall(call, _("comparison is not allowed for expressions"));
 
-    if (xarray || yarray) {
-	if (xarray && yarray) {
-	    if (!conformable(x, y))
-		errorcall(call, _("non-conformable arrays"));
-	    PROTECT(dims = getAttrib(x, R_DimSymbol));
-	}
-	else if (xarray) {
-	    PROTECT(dims = getAttrib(x, R_DimSymbol));
-	}
-	else /*(yarray)*/ {
-	    PROTECT(dims = getAttrib(y, R_DimSymbol));
-	}
-	PROTECT(xnames = getAttrib(x, R_DimNamesSymbol));
-	PROTECT(ynames = getAttrib(y, R_DimNamesSymbol));
+        /* At this point, x and y are both atomic or vector list */
+
+        if (LENGTH(x) <= 0 || LENGTH(y) <= 0) {
+            UNPROTECT(2);
+            return allocVector(LGLSXP,0);
+        }
+
+        mismatch = FALSE;
+        xarray = isArray(x);
+        yarray = isArray(y);
+        xts = isTs(x);
+        yts = isTs(y);
+        if (nx > 0 && ny > 0)
+            mismatch = ((nx > ny) ? nx % ny : ny % nx) != 0;
+
+        if (xarray || yarray) {
+            if (xarray && yarray) {
+                if (!conformable(x, y))
+                    errorcall(call, _("non-conformable arrays"));
+                PROTECT(dims = getAttrib(x, R_DimSymbol));
+            }
+            else if (xarray) {
+                PROTECT(dims = getAttrib(x, R_DimSymbol));
+            }
+            else /*(yarray)*/ {
+                PROTECT(dims = getAttrib(y, R_DimSymbol));
+            }
+            PROTECT(xnames = getAttrib(x, R_DimNamesSymbol));
+            PROTECT(ynames = getAttrib(y, R_DimNamesSymbol));
+        }
+        else {
+            PROTECT(dims = R_NilValue);
+            PROTECT(xnames = getAttrib(x, R_NamesSymbol));
+            PROTECT(ynames = getAttrib(y, R_NamesSymbol));
+        }
+
+        if (xts || yts) {
+            if (xts && yts) {
+                if (!tsConform(x, y))
+                    errorcall(call, _("non-conformable time series"));
+                PROTECT(tsp = getAttrib(x, R_TspSymbol));
+                PROTECT(klass = !objx ? R_NilValue :getAttrib(x,R_ClassSymbol));
+            }
+            else if (xts) {
+                if (length(x) < length(y))
+                    ErrorMessage(call, ERROR_TSVEC_MISMATCH);
+                PROTECT(tsp = getAttrib(x, R_TspSymbol));
+                PROTECT(klass = !objx ? R_NilValue :getAttrib(x,R_ClassSymbol));
+            }
+            else /*(yts)*/ {
+                if (length(y) < length(x))
+            	ErrorMessage(call, ERROR_TSVEC_MISMATCH);
+                PROTECT(tsp = getAttrib(y, R_TspSymbol));
+                PROTECT(klass = !objy ? R_NilValue :getAttrib(y,R_ClassSymbol));
+            }
+        }
+
+        if (mismatch)
+            warningcall (call, 
+          _("longer object length is not a multiple of shorter object length"));
+
+        if (typeof_x == STRSXP || typeof_y == STRSXP) {
+            if (typeof_x != STRSXP) REPROTECT(x = coerceVector(x, STRSXP), xpi);
+            if (typeof_y != STRSXP) REPROTECT(y = coerceVector(y, STRSXP), ypi);
+        }
+        else if (typeof_x == CPLXSXP || typeof_y == CPLXSXP) {
+            if (typeof_x!=CPLXSXP) REPROTECT(x = coerceVector(x, CPLXSXP), xpi);
+            if (typeof_y!=CPLXSXP) REPROTECT(y = coerceVector(y, CPLXSXP), ypi);
+            if (code != EQOP)
+                errorcall (call, _("invalid comparison with complex values"));
+        }
+        else if (typeof_x == REALSXP || typeof_y == REALSXP) {
+            if (typeof_x!=REALSXP) REPROTECT(x = coerceVector(x, REALSXP), xpi);
+            if (typeof_y!=REALSXP) REPROTECT(y = coerceVector(y, REALSXP), ypi);
+        }
+        else if (typeof_x == INTSXP || typeof_y == INTSXP) {
+            /* NOT isInteger, since it needs to be true for factors with
+               VARIANT_UNCLASS_FLAG.  Assumes LOGICAL same as INTEGER. */
+            if (typeof_x != INTSXP) REPROTECT(x = coerceVector(x, INTSXP), xpi);
+            if (typeof_y != INTSXP) REPROTECT(y = coerceVector(y, INTSXP), ypi);
+        }
+        else if (typeof_x == RAWSXP || typeof_y == RAWSXP) {
+            if (typeof_x != RAWSXP) REPROTECT(x = coerceVector(x, RAWSXP), xpi);
+            if (typeof_y != RAWSXP) REPROTECT(y = coerceVector(y, RAWSXP), ypi);
+        }
+        else 
+            errorcall (call, 
+                       _("comparison of these types is not implemented"));
     }
-    else {
-	PROTECT(dims = R_NilValue);
-	PROTECT(xnames = getAttrib(x, R_NamesSymbol));
-	PROTECT(ynames = getAttrib(y, R_NamesSymbol));
-    }
-    if (xts || yts) {
-	if (xts && yts) {
-	    if (!tsConform(x, y))
-		errorcall(call, _("non-conformable time series"));
-	    PROTECT(tsp = getAttrib(x, R_TspSymbol));
-	    PROTECT(klass = !objx ? R_NilValue : getAttrib(x, R_ClassSymbol));
-	}
-	else if (xts) {
-	    if (length(x) < length(y))
-		ErrorMessage(call, ERROR_TSVEC_MISMATCH);
-	    PROTECT(tsp = getAttrib(x, R_TspSymbol));
-	    PROTECT(klass = !objx ? R_NilValue : getAttrib(x, R_ClassSymbol));
-	}
-	else /*(yts)*/ {
-	    if (length(y) < length(x))
-		ErrorMessage(call, ERROR_TSVEC_MISMATCH);
-	    PROTECT(tsp = getAttrib(y, R_TspSymbol));
-	    PROTECT(klass = !objy ? R_NilValue : getAttrib(y, R_ClassSymbol));
-	}
-    }
-    if (mismatch)
-	warningcall(call, _("longer object length is not a multiple of shorter object length"));
 
     if (typeof_x == STRSXP || typeof_y == STRSXP) {
-	REPROTECT(x = coerceVector(x, STRSXP), xpi);
-	REPROTECT(y = coerceVector(y, STRSXP), ypi);
         switch (VARIANT_KIND(variant)) {
         case VARIANT_AND: 
             ans = string_relop_and (code, negate, x, y); 
@@ -637,30 +654,7 @@ SEXP attribute_hidden R_relop (SEXP call, SEXP op, SEXP x, SEXP y,
             break;
         }
     }
-    else {
-        if (typeof_x == CPLXSXP || typeof_y == CPLXSXP) {
-            REPROTECT(x = coerceVector(x, CPLXSXP), xpi);
-            REPROTECT(y = coerceVector(y, CPLXSXP), ypi);
-            if (code != EQOP)
-                errorcall(call, _("invalid comparison with complex values"));
-        }
-        else if (typeof_x == REALSXP || typeof_y == REALSXP) {
-            REPROTECT(x = coerceVector(x, REALSXP), xpi);
-            REPROTECT(y = coerceVector(y, REALSXP), ypi);
-        }
-        else if (typeof_x == INTSXP || typeof_y == INTSXP) {
-            /* NOT isInteger, since it needs to be true for factors with
-               VARIANT_UNCLASS_FLAG.  Assumes LOGICAL same as INTEGER. */
-            REPROTECT(x = coerceVector(x, INTSXP), xpi);
-            REPROTECT(y = coerceVector(y, INTSXP), ypi);
-        }
-        else if (typeof_x == RAWSXP || typeof_y == RAWSXP) {
-            REPROTECT(x = coerceVector(x, RAWSXP), xpi);
-            REPROTECT(y = coerceVector(y, RAWSXP), ypi);
-        }
-        else 
-            errorcall(call, _("comparison of these types is not implemented"));
-
+    else /* not strings */ {
         helpers_op_t codeop = (code<<1) | negate;
         switch (VARIANT_KIND(variant)) {
         case VARIANT_AND: 
@@ -680,27 +674,28 @@ SEXP attribute_hidden R_relop (SEXP call, SEXP op, SEXP x, SEXP y,
             task_relop (codeop, ans, x, y); 
             break;
         }
-
     }
 
     PROTECT(ans);
+
+    /* Tack on dims, names, ts stuff, if necessary. */
+
     if (dims != R_NilValue) {
-	setAttrib(ans, R_DimSymbol, dims);
-	if (xnames != R_NilValue)
-	    setAttrib(ans, R_DimNamesSymbol, xnames);
-	else if (ynames != R_NilValue)
-	    setAttrib(ans, R_DimNamesSymbol, ynames);
+        setAttrib(ans, R_DimSymbol, dims);
+        if (xnames != R_NilValue)
+            setAttrib(ans, R_DimNamesSymbol, xnames);
+        else if (ynames != R_NilValue)
+            setAttrib(ans, R_DimNamesSymbol, ynames);
     }
-    else {
-	if (length(ans) == length(xnames))
-	    setAttrib(ans, R_NamesSymbol, xnames);
-	else if (length(ans) == length(ynames))
-	    setAttrib(ans, R_NamesSymbol, ynames);
-    }
+    else if (xnames != R_NilValue && LENGTH(ans) == LENGTH(xnames))
+        setAttrib(ans, R_NamesSymbol, xnames);
+    else if (ynames != R_NilValue && LENGTH(ans) == LENGTH(ynames))
+        setAttrib(ans, R_NamesSymbol, ynames);
+
     if (xts || yts) {
-	setAttrib(ans, R_TspSymbol, tsp);
-	setAttrib(ans, R_ClassSymbol, klass);
-	UNPROTECT(2);
+        setAttrib(ans, R_TspSymbol, tsp);
+        setAttrib(ans, R_ClassSymbol, klass);
+        UNPROTECT(2);
     }
 
     UNPROTECT(6);
