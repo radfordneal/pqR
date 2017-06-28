ae53094a60724f85adecee3b65fb28e35eea767e
Radford Neal <radfordneal@gmail.com>
2017-06-28 16:55:26 -0400

    update comments in sggc-app.h

--- src/extra/sggc/sggc-app.h
+++ src/extra/sggc/sggc-app.h
@@ -124,9 +124,9 @@ sggc_nchunks_t Rf_nchunks (int type /* SEXPTYPE */, unsigned length);
 
       Symbol:             Primitive:        Environment:    External pointer:
         info, lastbinding   info, padding     info, frame     info, unused/shift
-        value, hash         C-function        enclos, hastab  prot, tag
-        lastenv, symbits2   fast-C-function   envsymbits      external ptr(+pad)
-        symbits             64 bits of info   padding         padding
+        value, hash         C-function        enclos, hashtab prot, tag
+        lastenv, symbits2   fast-C-function   envsymbits2     external ptr(+pad)
+        symbits             64 bits of info   envsymbits      padding
         = 32 bytes          padding if 32-bit = 32 bytes      = 32 bytes
           (2 chunks)        = 32 bytes          (2 chunks)      (2 chunks)
                               (2 chunks)
@@ -223,10 +223,10 @@ sggc_nchunks_t Rf_nchunks (int type /* SEXPTYPE */, unsigned length);
         attrib              attrib            attrib           attrib
         length, padding     length, padding   length, padding  length, padding
         lastbinding         C-function        frame            external ptr
-        value               fast-C-function   encos            prot
+        value               fast-C-function   enclos           prot
         hash, lastenv       64 bits of info   hashtab          tag
-        symbits2            = 48 bytes        envsymbits       = 48 bytes
-        symbits               (3 chunks)      padding            (3 chunks)
+        symbits2            = 48 bytes        envsymbits2       = 48 bytes
+        symbits               (3 chunks)      envsymbits          (3 chunks)
         = 64 bytes                            = 64 bytes
           (4 chunks)                            (4 chunks)
 */
@@ -305,10 +305,10 @@ sggc_nchunks_t Rf_nchunks (int type /* SEXPTYPE */, unsigned length);
       Symbol:             Primitive:         Environment:    External pointer:
         info, cptr          info, cptr         info, cptr      info, cptr
         lastbinding         C-function         frame           external ptr
-        value               fast-C-function    encos           prot
+        value               fast-C-function    enlcos          prot
         hash, lastenv       64 bits of info    hashtab         tag
-        symbits2            = 32 bytes         envsymbits      = 32 bytes
-        symbits               (2 chunks)       padding           (2 chunks)
+        symbits2            = 32 bytes         envsymbits2     = 32 bytes
+        symbits               (2 chunks)       envsymbits        (2 chunks)
         = 48 bytes                             = 48 bytes
           (3 chunks)                             (3 chunks)
 */
@@ -390,8 +390,8 @@ sggc_nchunks_t Rf_nchunks (int type /* SEXPTYPE */, unsigned length);
         attrib, length      attrib, length       attrib, length  attrib, length
         lastbinding, value  C-function, fastfun  frame, enclos   prot, tag
         hash, lastenv       64 bits of info      hashtab, pad    xptr, padding
-        symbits2            = 32 bytes         envsymbits      = 32 bytes
-        symbits                 (2 chunks)       padding           (2 chunks)
+        symbits2            = 32 bytes           envsymbits2     = 32 bytes
+        symbits                 (2 chunks)       envsymbits        (2 chunks)
         = 48 bytes                               = 48 bytes
           (3 chunks)                               (3 chunks)
 */
