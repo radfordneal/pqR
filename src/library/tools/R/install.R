330226f8c7a5e81d73066e3b454f9e91c00b54ff
Radford Neal <radfordneal@gmail.com>
2017-07-13 17:51:50 -0400

    copyright updates

--- src/library/tools/R/install.R
+++ src/library/tools/R/install.R
@@ -1,5 +1,6 @@
 #  File src/library/tools/R/install.R
 #  Part of the R package, http://www.R-project.org
+#  Modifications for pqR Copyright (c) 2017 Radford M. Neal.
 #
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
