8a6ce6d4a8cc8eac8d627f6ff7be952a21a529c1
Radford Neal <radfordneal@gmail.com>
2017-10-10 21:11:27 -0400

    modify trimws to slightly improve speed

--- src/library/base/R/strwrap.R
+++ src/library/base/R/strwrap.R
@@ -188,16 +188,12 @@ function(x, y, style = c("table", "list"),
     r
 }
 
-# From R-3.2.0:
+# From R-3.2.0, modified to improve performance.
 
-trimws <-
-function(x, which = c("both", "left", "right"))
+trimws <- function(x, which = c("both", "left", "right"))
 {
     which <- match.arg(which)
-    mysub <- function(re, x) sub(re, "", x, perl = TRUE)
-    if(which == "left")
-        return(mysub("^[ \t\r\n]+", x))
-    if(which == "right")
-        return(mysub("[ \t\r\n]+$", x))
-    mysub("[ \t\r\n]+$", mysub("^[ \t\r\n]+", x))
+    if (which != "right") x <- sub("^[ \t\r\n]+", "", x, perl=TRUE)
+    if (which != "left")  x <- sub("[ \t\r\n]+$", "", x, perl=TRUE)
+    x
 }
