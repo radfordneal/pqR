f2b2d239322451b346290a224dac32c310660b51
Radford Neal <radfordneal@gmail.com>
2017-10-10 21:00:32 -0400

    added trimws

--- src/library/base/R/strwrap.R
+++ src/library/base/R/strwrap.R
@@ -187,3 +187,17 @@ function(x, y, style = c("table", "list"),
     }
     r
 }
+
+# From R-3.2.0:
+
+trimws <-
+function(x, which = c("both", "left", "right"))
+{
+    which <- match.arg(which)
+    mysub <- function(re, x) sub(re, "", x, perl = TRUE)
+    if(which == "left")
+        return(mysub("^[ \t\r\n]+", x))
+    if(which == "right")
+        return(mysub("[ \t\r\n]+$", x))
+    mysub("[ \t\r\n]+$", mysub("^[ \t\r\n]+", x))
+}
