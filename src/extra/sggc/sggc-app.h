3219226e9ace4f33ca781155c98611aaae7bcda0
Radford Neal <radfordneal@gmail.com>
2018-07-19 18:11:25 -0400

    get rid of surplus semicolon

--- src/extra/sggc/sggc-app.h
+++ src/extra/sggc/sggc-app.h
@@ -486,7 +486,7 @@ sggc_nchunks_t Rf_nchunks (int type /* SEXPTYPE */, unsigned length);
   0, 1, 2, 3, 4, 5, \
   0, 1, 2, 3, 4, 5, \
   0, 1, 2, 3, 4, 5  \
-};
+}
 
 #define SGGC_KIND_UNCOLLECTED \
 { 0, 0, 0, 0, 0, 1, \
