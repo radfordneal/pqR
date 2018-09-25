3a19deeb773600047b4d28a044395d785b777b9d
Radford Neal <radfordneal@gmail.com>
2018-09-25 14:49:13 -0400

    more extensive matprod tests

--- tests/matprod.R
+++ tests/matprod.R
@@ -1,7 +1,7 @@
 # Test matrix multiplication, with %*%, crossprod, and tcrossprod, by
 # BLAS and 'matprod' routines.
 #
-# Added for pqR, 2013, Radford M. Neal.
+# Added for pqR, 2013, 2018 Radford M. Neal.
 
 # Check matrix multiplication with various sizes of matrices, setting
 # matrix elements to random values that are integer multiples of 1/8 so
@@ -11,8 +11,13 @@
 #
 # Returns the last (largest) result matrix.
 
-check_matprod <- function ()
-{  
+check_matprod <- function (print=TRUE)
+{ 
+  if (print) 
+  { cat("\n")
+    print(options()[c("mat_mult_with_BLAS","helpers_disable")])
+  }
+
   # Matrix multiply the hard way, to check results.
 
   matmult <- function (A,B)
@@ -29,43 +34,88 @@ check_matprod <- function ()
     C
   }
 
+  # Do checks with given matrix sizes.
+
+  check <- function (n, m, k)
+  { A <- matrix (rgeom(n*k,0.1)/8, n, k)
+    B <- matrix (rgeom(k*m,0.1)/8, k, m)
+    C <- matmult(A,B)
+    At <- t(A)
+    Bt <- t(B)
+    stopifnot(identical(A%*%B,C))
+    stopifnot(identical(crossprod(At,B),C))
+    stopifnot(identical(tcrossprod(A,Bt),C))
+    if (n==m) 
+    { D <- matmult(At,A)
+      stopifnot(identical(crossprod(A),D))
+      stopifnot(identical(tcrossprod(At),D))
+    }
+    C
+  }
+
   # Set seed to get consistent results.
 
   set.seed(1)
 
-  # Try various sizes.
+  # Try various sizes systematically.
 
   for (n in 0:11)
   { for (m in 0:11)
     { for (k in 0:11)
-      { A <- matrix (rgeom(n*k,0.1)/8, n, k)
-        B <- matrix (rgeom(k*m,0.1)/8, k, m)
-        C <- matmult(A,B)
-        At <- t(A)
-        Bt <- t(B)
-        stopifnot(identical(A%*%B,C))
-        stopifnot(identical(crossprod(At,B),C))
-        stopifnot(identical(tcrossprod(A,Bt),C))
-        if (n==m) 
-        { D <- matmult(At,A)
-          stopifnot(identical(crossprod(A),D))
-          stopifnot(identical(tcrossprod(At),D))
-        }
+      { C <- check(n,m,k)
       }
     }
   }
 
-  C
+  if (print)
+  { cat("\n")
+    print(C)
+  }
+
+  # Try various sizes randomly.  Can increase loop count for more testing
+  # by setting the environment variable R_MATPROD_TEST_COUNT (minimum 200).
+
+  count <- as.integer(Sys.getenv("R_MATPROD_TEST_COUNT"))
+  if (is.na(count) || count < 200) count <- 200
+
+  for (i in 1..count)
+  {  n <- if (runif(1)<0.6) 15*rpois(1,10) + rpois(1,10) 
+          else if (runif(1)>0.93) 50*rpois(1,12) + rpois(1,30) 
+          else rpois(1,5)
+     m <- if (runif(1)<0.6) 15*rpois(1,10) + rpois(1,10) 
+          else if (runif(1)>0.93) 50*rpois(1,12) + rpois(1,30) 
+          else rpois(1,5)
+     k <- if (runif(1)<0.6) 15*rpois(1,10) + rpois(1,10) 
+          else if (runif(1)>0.93) 50*rpois(1,12) + rpois(1,30) 
+          else rpois(1,5)
+     if (n > 1200) n <- 1200
+     if (m > 1200) m <- 1200
+     if (k > 1200) k <- 1200
+     # Print n,m,k, but only for the first 200, so increasing the loop
+     # repetitions won't invalidate saved output.
+     if (print && i <= 200) print(c(n,m,k))
+     check(n,m,k)
+  }
 }
 
-# Check matrix products using BLAS and using 'matprod' routines.
+# Check matrix products using BLAS and using 'matprod' routines, with
+# or without helper threads enabled.
 
-sv <- getOption("mat_mult_with_BLAS")
+sv <- options()[c("mat_mult_with_BLAS","helpers_disable")]
 
-options(mat_mult_with_BLAS=F)
-print(check_matprod())
+options(mat_mult_with_BLAS=FALSE,helpers_disable=FALSE)
+check_matprod()
 
-options(mat_mult_with_BLAS=T)
-print(check_matprod())
+options(mat_mult_with_BLAS=FALSE,helpers_disable=TRUE)
+check_matprod()
 
-options(mat_mult_with_BLAS=sv)
+if (identical(Sys.getenv("R_MATPROD_TEST_BLAS"),"TRUE")) {
+
+    options(mat_mult_with_BLAS=TRUE,helpers_disable=FALSE)
+    check_matprod(print=FALSE)
+
+    options(mat_mult_with_BLAS=TRUE,helpers_disable=TRUE)
+    check_matprod(print=FALSE)
+}
+
+options(sv)
