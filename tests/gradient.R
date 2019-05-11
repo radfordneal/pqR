ff40736e7741f0c63ec1a1326976b48b153e6cae
Radford Neal <radfordneal@gmail.com>
2019-05-10 21:14:44 -0400

    another gradient test

--- tests/gradient.R
+++ tests/gradient.R
@@ -2251,6 +2251,7 @@ pr("start")
 with gradient (a=c(3,9,7,4,2,6,5,8)) {
   pr("A"); x <- cumsum(a)
   pr("B"); y <- x^2 + sin(x)
-  pr("C"); list(x,y)
+  pr("C"); z <- sqrt(dnorm(x,0,x+1))
+  pr("D"); list(x,y,z)
 }
 pr("end")
